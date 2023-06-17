0c00e15448325c8a02a8b5170e038efa191cdf40
Aaron Ehresman <aaron.ehr@gmail.com>
2023-06-16 18:07:09 -0700

    test rss feed

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -6,8 +6,8 @@ const matter = require('gray-matter')
 async function generate() {
   const feed = new RSS({
     title: 'Aaron',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    site_url: `https://${process.env.VERCEL_URL}`,
+    feed_url: `https://${process.env.VERCEL_URL}/feed.xml`
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
